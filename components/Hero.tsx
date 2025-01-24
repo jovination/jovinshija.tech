9c17d111a5f3ba666607d20ad202f30ea82ac79c
jovination <jovination791@gmail.com>
2025-01-25 02:17:23 +0300

    few changes on hero sections

---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+
+function Hero() {
+    return(
+        <div className="mt-16 md:mt-20">
+        <div className="w-[370px] h-[730.28px] md:w-[550px] md:h-[881.59px] hero rounded-[36px] md:rounded-[46px] flex items-center justify-center ">
+            <div className="w-[358px] h-[623.11px] md:w-[532px] md:h-[778.39px] wrapper-hero rounded-[30px] md:rounded-[36px] py-[20px] px-[30px] md:py-[30px] md:px-[50px] gap-[28px] md:gap-[50px]">
+                <div
+                className="flex  justify-between "
+                >
+            <div className=" w-[92.67px] md:w-[137.33px] h-[4px]  rounded-full bg-[--green]"></div>
+            <div className="w-[92.67px] md:w-[137.33px]  h-[4px] rounded-full   bg-[--green]"></div>
+            <div className="w-[92.67px] md:w-[137.33px]  h-[4px] rounded-full   bg-[--grey]"></div>
+                  
+                </div>
+                <div className="w-full h-[126.11px] md:h-[84.61px]">
+                    <div>
+                        <div></div>    
+                        <div>
+                        </div>    
+                    </div>
+                    <div>
+                       <h1 className=" text-5xl md:text-7xl font-semibold ">
+                       I develop experiences that work for you.
+                      </h1> 
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>  
+        </div>  
+    )
+}
+export default Hero
---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import { Home, MessageCircle } from 'lucide-react'
+
+function Navbar() {
+  return (
+    <nav className="w-[309px] h-[54px] navbar m-4 rounded-full p-4 flex items-center">
+      <div className="w-full h-auto flex items-center gap-2">
+        <Link href="/" className="hover:opacity-80 transition-opacity mr-7">
+          <Home size={20} />
+        </Link>
+        <div className="flex w-[243px] gap-[24px] items-center">
+          <Link href="/projects" className="hover:text-gray-700 transition-colors">
+            Projects
+          </Link>
+          <Link href="/blog" className="hover:text-gray-700 transition-colors">
+            Blog
+          </Link>
+          <Link
+            href="/contact"
+            className="w-[94px] h-[46px] rounded-full bg-white text-[--background] 
+                       flex items-center justify-center 
+                       hover:bg-[--primary] transition-colors
+                       hover:text-white"
+                      
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
