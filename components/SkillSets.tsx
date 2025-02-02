e4521fe027c407f47c70602cf0fbf2a81c948614
jovination <jovination791@gmail.com>
2025-02-02 19:35:16 +0300

    few changes

--- components/SkillSets.tsx
+++ components/SkillSets.tsx
@@ -1,7 +1,7 @@
 import TechStack from "@/components/TechStack"
 function SkillSets(){
     return(
-        <div className="mt-40 space-y-20 mb-10">
+        <div className="mt-40 space-y-16 mb-10">
    <div className="flex flex-col items-center gap-6">
                 <h2 className=" text-3xl md:text-5xl font-medium">My Skills</h2>
                 <p className="text-[--grey01] text-center font-medium">
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -2,6 +2,11 @@
 
 import { FiFigma } from "react-icons/fi"
 import { SiFramer, SiAdobephotoshop } from "react-icons/si"
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaReact, FaNodeJs, FaDatabase } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+
+
 import type { ReactNode } from "react"
 
 interface Skill {
@@ -17,23 +22,42 @@ interface TechstackProps {
 
 const defaultSkills: Skill[] = [
   {
-    name: "Figma",
-    description: "Leading design tool",
-    icon: <FiFigma className="w-6 h-6" />,
+    name: "Reactjs & Nextjs",
+    description: "Building fast, dynamic and SE0 optimized web App",
+    icon: <FaReact className="w-6 h-6" />,
     proficiency: 90,
   },
   {
-    name: "Framer",
-    description: "No-code website builder",
-    icon: <SiFramer className="w-6 h-6" />,
-    proficiency: 70,
+    name: "Figma & UI/UX Design",
+    description: "Creating high-fidelity wireframe and interactive prototypes.",
+    icon: <FiFigma className="w-6 h-6" />,
+    proficiency: 85,
   },
   {
-    name: "Adobe Photoshop",
-    description: "Raster graphics editor",
-    icon: <SiAdobephotoshop className="w-6 h-6" />,
+    name: "Node.js",
+    description: "Building RESTful APIs and microservices using Express.js",
+    icon: <FaNodeJs className="w-6 h-6" />,
     proficiency: 60,
   },
+  {
+    name: "Tailwind CSS & ShadCN UI",
+    description: "Building fast, Responsive and modern UI components.",
+    icon: <RiTailwindCssFill className="w-6 h-6" />,
+    proficiency: 85,
+  },
+  {
+    name: "Supabase & PostgressSQL",
+    description: "Managing structures and unstrucrured data Efficiently.",
+    icon: <FaDatabase className="w-5 h-5" />,
+    proficiency: 65,
+  },
+  {
+    name: "Git & GitHub",
+    description: "Managing code repositories with Git for version control.",
+    icon: <IoLogoGithub className="w-6 h-6" />,
+    proficiency: 75,
+  },
+ 
 ]
 
 function TechStack({ skills = defaultSkills }: TechstackProps) {
@@ -43,27 +67,25 @@ function TechStack({ skills = defaultSkills }: TechstackProps) {
         {skills.map((skill) => (
           <div 
             key={skill.name} 
-            className="relative w-[370px] md:w-[550px] h-[80px] hero rounded-[20px] overflow-hidden"
+            className="relative w-[370px] md:w-[550px] h-[90px] hero rounded-[20px] overflow-hidden"
           >
-            {/* Black background */}
-            <div className="absolute inset-0 bg-neutral-900" />
+            <div className="absolute inset-0 " />
 
             <div 
               className="absolute inset-0 bg-[--primary]" 
               style={{ width: `${skill.proficiency}%` }} 
             />
 
-            {/* Content overlay */}
             <div className="relative h-full flex items-center p-4 gap-3">
-              <div className="flex-shrink-0 w-12 h-12 bg-black/20 rounded-xl flex items-center justify-center">
+              <div className="flex-shrink-0 w-12 h-12 bg-black/70 rounded-xl flex items-center justify-center">
                 {skill.icon}
               </div>
               <div className="flex-grow">
                 <h3 className="font-medium text-white">{skill.name}</h3>
-                <p className="text-[#DEDEDE] text-sm">{skill.description}</p>
+                <p className="text-[#B8B8FE] text-sm font-medium">{skill.description}</p>
               </div>
-              <div className="flex-shrink-0">
-                <span className="text-white font-medium">{skill.proficiency}%</span>
+              <div className="flex-shrink-0 bg-black/30 py-[4px] px-[8px] rounded-full">
+                <span className="text-sm text-white font-medium">{skill.proficiency}%</span>
               </div>
             </div>
           </div>
