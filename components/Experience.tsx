1296a2d83d093bba4753c388648bee296e15d5e5
jovination <jovination791@gmail.com>
2025-02-06 23:59:17 +0300

    few changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,9 +1,15 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
 function Experience(){
 
     return(
-        <div className="mt-24 w-[370px] h-[135px]  md:w-[550px] md:h-[172] border border-[#404141] rounded-[30px]  md:rounded-[36px]  px-[48px] py-[40px] md:px-[60px] md:py-[50px] flex items-center gap-[20px]"> 
+        <div className="mt-24 w-[370px] h-[135px]  md:w-[550px] md:h-[172] border border-[#404141] rounded-[30px]  md:rounded-[36px]  px-[48px] py-[40px] md:px-[60px] md:py-[50px] flex items-center gap-[40px]"> 
        <div>
-        <p className="text-7xl font-semibold">3</p>
+       <p className="text-7xl font-semibold relative inline-block">
+     3
+  <AiOutlinePlus className="text-3xl text-[--grey01] absolute top-0 left-13 -translate-x-1/2" />
+    </p>
+
        </div>
        <div className="h-full flex flex-col justify-start">
        <p className="md:leading-1 text-sm md:text-base text-white font-medium">
