1d043f236823123bb1baaf0befefc3f33d8b01b3
jovination <jovination791@gmail.com>
2025-02-07 23:51:31 +0300

    few changes

---
+++ components/ContactButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { LuArrowUpRight } from "react-icons/lu";
+
+
+const ContactButton = ({ 
+  href = "#", 
+  children = "Contact me", 
+  className = "",
+  iconClassName = ""
+}) => {
+  return (
+    <div className={`bg-[--primary] flex items-center justify-end rounded-full gap-6 pr-[2px] py-[2px] pl-[25px] w-fit ${className}`}>
+      <a 
+        href={href} 
+        className="text-[17px] font-medium"
+      >
+        {children}
+      </a>
+      <div className="w-[52px] h-[52px] bg-[--background] rounded-full flex items-center justify-center">
+        <LuArrowUpRight 
+          className={`text-white w-[20px] h-[20px] ${iconClassName}`} 
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ContactButton;
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,11 @@
 import { FaPlus } from "react-icons/fa";
 import Image from "next/image"
+import ContactButton from "@/components/ContactButton"
 
 function Experience(){
 
     return(
-    <div className="flex flex-col space-y-1"> 
+    <div className="flex flex-col gap-2"> 
     <div className="mt-16 w-[370px] h-[135px]  md:w-[550px] md:h-[172] border border-[#404141] rounded-[30px]  md:rounded-[36px]  px-[48px] py-[40px] md:px-[60px] md:py-[50px] flex items-center gap-[40px]"> 
      <div>
        <p className="text-7xl font-semibold relative inline-block">
@@ -21,7 +22,7 @@ function Experience(){
       </p>
        </div>
         </div>
-        <div className="flex w-[370px] md:w-[550px] items-center justify-between" >
+        <div className="flex w-[370px] md:w-[550px] items-center justify-between mb-12" >
         <div className="w-[183px] md:w-[273px] h-[183px] md:h-[273px]  rounded-[30px] md:rounded-[36px] relative overflow-hidden">
           <Image
             src="/web.png"
@@ -31,12 +32,12 @@ function Experience(){
           />
 
           </div>
-          <div
-            className="w-[183px] md:w-[273px] h-[183px] md:h-[273px] bg-[--primary] rounded-[30px] md:rounded-[36px] p-[32px] md:p-[40px] flex flex-col justify-between "
+          <div className="w-[183px] md:w-[273px] h-[183px] md:h-[273px] bg-[--primary] rounded-[30px] md:rounded-[36px] p-[32px] md:p-[40px] flex flex-col justify-between  "
           >
               <Image
               className="hidden md:flex"
               src="./SVG.svg"
+              alt="icon"
               width={190}
               height={40}
                />
@@ -49,13 +50,14 @@ function Experience(){
              <p className="md:leading-1 text-sm md:text-base text-white font-medium">
              Client satisfaction rate
 
-built on trust and results.        
-</p>
+            built on trust and results.        
+                  </p>
             </div>
 
           </div>
-        
         </div>
+        <ContactButton /> 
+
         </div>
     )
 }
