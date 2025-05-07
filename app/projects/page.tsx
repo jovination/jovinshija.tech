926d689e96252669ec1d1d0459195d72109be3fd
Jovination <jovination791@gmail.com>
2025-05-07 17:20:54 +0300

    updates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,12 @@
 
 import Work from "@/components/Work";
 import ContactPage from "@/components/ContactPage"
+import Navbar from "@/components/Navbar";
 
 function ProjectPage(){
     return (
         <div className="flex flex-col  items-center min-h-screen mx-auto">
+        <Navbar />
         <Work />
         <ContactPage />
         </div>
